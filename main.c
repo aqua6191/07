dc54aa6c6faa3e64e337511d0df70438dde19331
양지원 <101974118+aqua6191@users.noreply.github.com>
2023-10-19 16:38:33 +0900

    06-2

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int all_files;
+static int all_files; //static 삭제하면오류 해결  
 extern void sub();
-int main(void){
-    sub();
-    printf("%d\n", all_files);
-    system("PAUSE");
-    return 0;
+int main(void)
+{
+ sub();
+ printf("%d\n", all_files);
+ system("PAUSE");
+ return 0;
 }
