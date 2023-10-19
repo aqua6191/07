e30bf3e81d4d22bb81c9590a2866067811691fb5
양지원 <101974118+aqua6191@users.noreply.github.com>
2023-10-19 15:44:53 +0900

    03

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
-    int i;
-    int temp=1;
+int inc(int counter);
+int main(int argc, char *argv[]){
+    int i =10;
     
-    for(i=0;i<5;i++)
-    {
-     
-     printf("temp = %d\n", temp);
-     temp++;
-     }                
+    printf("함수 호출전 i= %d\n",i);
+    i= inc(i); //반환 
+    printf("함수 호출후 i= %d\n",i);
+    
     system("PAUSE");
-    return 0;
-    }
+    return 0;}
+    
+    int inc(int counter){
+    counter++;
+    return counter;}
+    
